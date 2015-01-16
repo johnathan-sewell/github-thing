e32bf41b84642f14ac523812b46f1c0b965f323b
Jonathan Sewell <jonathans@brandwatch.com>
2015-01-16 15:42:12 +0000

    body-parser

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 var config = require('./config');
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var express = require('express')
 var app = express()
+app.use(bodyParser.json())
 
 function log(req){
-  console.log('%s request handeled for %s', req.method, req.url);
+  console.log('%s request handled for %s', req.method, req.url);
 }
+
 app.get('/', function(req, res) {
 	log(req);
 	res.send('Hello, please POST to /comment.\n');
 })
 
 app.post('/comment', function(req, res) {
+	log(req);
 	console.dir(req.body);	
 	res.send('Recieved your comment.\n');
 })
