ab429654a3f88d18d5c7896ae3f0ed23f6d0ae5d
Jonathan Sewell <jonathans@brandwatch.com>
2015-01-19 10:46:18 +0000

    add keen

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 var config = require('./config');
 var express = require('express');
 var bodyParser = require('body-parser');
+var Keen = require('keen.io');
 
 var express = require('express')
 var app = express()
 app.use(bodyParser.json())
 
-function log(req){
-  console.log('%s request handled for %s', req.method, req.url);
+function log(req) {
+	console.log('%s request handled for %s', req.method, req.url);
 }
+
+var keen = Keen.configure({
+	projectId: config.keen.projectId,
+	writeKey: config.keen.writeKey
+});
 
 app.get('/', function(req, res) {
 	log(req);
@@ -17,8 +23,20 @@ app.get('/', function(req, res) {
 
 app.post('/comment', function(req, res) {
 	log(req);
-	console.dir(req.body);	
-	res.send('Recieved your comment.\n');
+	console.dir(req.body);
+	
+
+	keen.addEvent('PullRequest', {
+		'type': 'pull_request_review_comment'
+	}, function(err) {
+		if (err) {
+			console.dir('Oh no, an error!', err);
+			res.send('Oh no, an error!\n');
+		} else {
+			console.log('Hooray, it worked!');
+			res.send('Hooray, it worked!\n');
+		}
+	});
 })
 
 var server = app.listen(config.listenPort, config.listenIP, function() {
